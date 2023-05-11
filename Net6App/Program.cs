33b217df31a604e6a957a78d0cbb033278c1f2c7
Eric Saldivar <saldivare1@rushenterprises.com>
2023-05-11 11:04:00 -0500

    Add project files.

---
+++ Net6App/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace HelloWorld
+{
+    class Program
+    {
+        //Entry point of our program
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = Console.ReadLine()!;
+            Console.WriteLine($"Hello {name}");
+
+        }
+    }
+}
